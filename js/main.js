db40b127330319d7f4d85aff1ffe1510de7b4414
Helen Yung <helen.yung.h@gmail.com>
2022-05-30 14:25:08 -0400

    carousel fixed between calls

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-let running = false;
-let timerId;
+const queue = []; //cancellable carousel
 
-document.querySelector('button').addEventListener('click', getDrink)
 function getDrink(){
-    if (running === true){
-        clearTimeout(timerId); 
-        running = false;
-    }
-    running = true;
-
     let drink = document.querySelector('input').value.split(' ').join('%20') 
     // user-entered value is assigned to this drink variable. Ensures search works even if input has spaces.
     let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
@@ -16,39 +8,45 @@ function getDrink(){
     fetch(url) 
         .then(res => res.json()) 
         .then(data => {
-            timerId = function(){
-                for (let i = 0; i < data.drinks.length; i++){
-                    setTimeout(function(){
-                        document.querySelector('#ingred').innerText = "";
-                        document.querySelector('#ingred').classList.add('hidden');
-                        document.querySelector('h2').innerText = data.drinks[i].strDrink;
-                        document.querySelector('img').src = data.drinks[i].strDrinkThumb;
-                        document.querySelector('img').alt = data.drinks[i].strDrink;
-                        document.querySelector('#ingredTitle').innerText = "Ingredients"
-                        let arr = [];
-                        for (let num=1; num <=15; num++){
-                            if (!(data.drinks[i][`strIngredient${num}`] === null || data.drinks[i][`strIngredient${num}`]=="")){
-                                arr.push(data.drinks[i][`strIngredient${num}`])
-                            } 
-                        }
-                        document.querySelector('#ingred').innerText = arr.join(', ')
-                        document.querySelector('#ingred').classList.remove('hidden')
-                        document.querySelector('#instructTitle').innerText = "Instructions"
-                        document.querySelector('#instructions').innerText = data.drinks[i].strInstructions;
-                        if (data.drinks[i].strAlcoholic == "Alcoholic"){
-                            document.querySelector('#alcohol').classList.remove('hidden')
-                        }else{
-                            document.querySelector('#alcohol').classList.add('hidden')
-                        }
-                    }, 5000*i); //delays for reading purposes
-                }
-            }
-            timerId();
-            running = false;
-        }) 
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+            for(let i = 0; i < data.drinks.length; i++){
+                const time = setTimeout(() => {
+                    document.querySelector('#ingred').innerText = "";
+                    document.querySelector('#ingred').classList.add('hidden');
+                    document.querySelector('h2').innerText = data.drinks[i].strDrink;
+                    document.querySelector('img').src = data.drinks[i].strDrinkThumb;
+                    document.querySelector('img').alt = data.drinks[i].strDrink;
+                    document.querySelector('#ingredTitle').innerText = "Ingredients"
+                    let arr = [];
+                    for (let num=1; num <=15; num++){
+                        if (!(data.drinks[i][`strIngredient${num}`] === null || data.drinks[i][`strIngredient${num}`]=="")){
+                            arr.push(data.drinks[i][`strIngredient${num}`])
+                        } 
+                    }
+                    document.querySelector('#ingred').innerText = arr.join(', ')
+                    document.querySelector('#ingred').classList.remove('hidden')
+                    document.querySelector('#instructTitle').innerText = "Instructions"
+                    document.querySelector('#instructions').innerText = data.drinks[i].strInstructions;
+                    if (data.drinks[i].strAlcoholic == "Alcoholic"){
+                        document.querySelector('#alcohol').classList.remove('hidden')
+                    }else{
+                        document.querySelector('#alcohol').classList.add('hidden')
+                    }
+                    queue.shift();
 
-    document.querySelectorAll('.terc').forEach(element => element.classList.toggle('hidden'));
+                }, 5000 * i);
+                
+                queue.push(time);
+            }
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+    
+        document.querySelectorAll('.terc').forEach(element => element.classList.toggle('hidden'));
 }
+
+document.querySelector('button').addEventListener('click', () => {
+	queue.forEach(timer => clearTimeout(timer));
+    queue.length = 0;
+    getDrink();
+})
